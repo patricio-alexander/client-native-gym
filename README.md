a98e6acb3c2a2742957ffe594f615ec129be288f
Patricio Alexander B <pabrisdev@gmail.com>
2024-05-12 18:50:48 -0500

    Agregando README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Iron Subscription: Tu App de Control de Pagos Mensuales para Gimnasios en React Native, Express, MySQL y React Native Paper
+
+![Captura 4](./.screenshot/screenshot4.png)
+![Captura 3](./.screenshot/screnshot3.png)
+![Captura 2](./.screenshot/screenshot2.png)
+![Captura 1](./.screenshot/screenshot1.png)
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,11 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import { AuthProvider } from "./context/AuthContext";
 import Routes from "./routes/Routes";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { CustomerContextProvider } from "./context/CustomerProvider";
 import { NavigationContainer } from "@react-navigation/native";
 import { useTheme } from "react-native-paper";
 
 const Index = () => {
-  const insets = useSafeAreaInsets();
   const theme = useTheme();
 
   return (
@@ -18,13 +16,12 @@ const Index = () => {
             flex: 1,
             justifyContent: "center",
             backgroundColor: theme.colors.background,
-            // Paddings to handle safe area
-            // paddingTop: insets.top,
-            // paddingBottom: insets.bottom,
-            // paddingLeft: insets.left,
-            // paddingRight: insets.right,
           }}
         >
+          <StatusBar
+            backgroundColor={theme.colors.background}
+            barStyle="dark-content"
+          />
           <NavigationContainer>
             <Routes />
           </NavigationContainer>
